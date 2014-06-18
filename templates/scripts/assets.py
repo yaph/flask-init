56b41160eb0d6c1a703cd822defcb30b5a72adfd
Ramiro Gómez <code@ramiro.org>
2014-06-18 21:57:55 +0200

    load vendor assets from cdn

--- templates/scripts/assets.py
+++ templates/scripts/assets.py
@@ -10,14 +10,12 @@ dir_static = os.path.join(
 static_env = Environment(dir_static)
 
 static_env.register('js_all', Bundle(
-    'vendor/bootstrap/js/bootstrap.min.js',
     'js/script.js',
     output='gen/all.js'))
 static_env['js_all'].build()
 
 # font-awesome is broken when included
 static_env.register('css_all', Bundle(
-    'vendor/bootstrap/css/bootstrap.css',
     'css/style.css',
     filters='cssmin', output='gen/all.css'))
 static_env['css_all'].build()
